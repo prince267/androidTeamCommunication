e4df87088cfc01ad7b53300d2ecdee633bc853dd
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-02-18 16:11:10 +0530

    TODO : sound during notification

--- NotifService.js
+++ NotifService.js
@@ -66,7 +66,7 @@ export default class NotifService {
       title: "Notification", // (optional)
       message: params.message, // (required)
       playSound: true, // (optional) default: true
-      soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+      soundName: 'telephone-ring-01a.mp3', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
       number: '10', // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
       // actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
     });
