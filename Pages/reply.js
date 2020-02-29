3a190f438f42f6bf3b6c24420f0a43496dcff69b
prince.kumar267@gmail.com <prince.kumar@arvindinternet.com>
2020-02-29 16:23:09 +0530

    minor changes

--- Pages/reply.js
+++ Pages/reply.js
@@ -21,40 +21,37 @@ function errorCB(err) {
 var db = openDatabase({ name: 'Team_lead.db', createFromLocation: 1 }, openCB, errorCB);
 
 export default class Reply extends React.Component {
-    static navigationOptions = {
-        title: "New",
-        //Sets Header text of Status Bar
-      };
-constructor(props){
+  static navigationOptions = {
+    title: "New",
+    //Sets Header text of Status Bar
+  };
+  constructor(props) {
     super(props);
     this.state = {
-        isReply:this.props.navigation.state.params.isReply || 0,
-        managerId: this.props.navigation.state.params.managerId || '', 
-        memberId: this.props.navigation.state.params.memberId || '',
-        reportId: '', 
-        reportText: '',
-        pastReportId:this.props.navigation.state.params.pastReportId|| ''
-      }
-      console.log(this.state.managerId," ***** ",this.state.memberId," ***** ",this.state.pastReportId)
-      console.log("is reply or new message",this.state.isReply)
-}
-  
+      isReply: this.props.navigation.state.params.isReply || 0,
+      managerId: this.props.navigation.state.params.managerId || '',
+      memberId: this.props.navigation.state.params.memberId || '',
+      reportId: '',
+      reportText: '',
+      pastReportId: this.props.navigation.state.params.pastReportId || ''
+    }
+  }
+
   onChangeText = (key, val) => {
     this.setState({ [key]: val })
   }
   sendReply = async () => {
-      const {navigate}=this.props.navigation
-      var currentTime=new Date().getHours().toString() + new Date().getMinutes().toString();
-    const { managerId,memberId,reportId,reportText,pastReportId } = this.state
-      console.log(managerId," *** ",memberId," *** ",reportId," *** ",reportText," *** ",pastReportId)
-      if (managerId) {
-        if (memberId) {
-          if (reportId) {
-              if(reportText){
-            db.transaction(function(tx) {
+    const { navigate } = this.props.navigation
+    var currentTime = new Date().getHours().toString() + new Date().getMinutes().toString();
+    const { managerId, memberId, reportId, reportText, pastReportId } = this.state
+    if (managerId) {
+      if (memberId) {
+        if (reportId) {
+          if (reportText) {
+            db.transaction(function (tx) {
               tx.executeSql(
                 'INSERT INTO seniorManagerReporting (managerId,memberId,reportId,reportText,refPastReportId,arrivalDateTime,seenDateTime,seenOrUnseen) VALUES (?,?,?,?,?,?,?,?)',
-                [managerId, memberId, reportId, reportText,pastReportId, currentTime,null,0],
+                [managerId, memberId, reportId, reportText, pastReportId, currentTime, null, 0],
                 (tx, results) => {
                   console.log('Results', results.rowsAffected);
                   if (results.rowsAffected > 0) {
@@ -86,74 +83,74 @@ constructor(props){
         alert('Please fill Member Id');
       }
     }
-    else{
-        alert('Please fill Manager Id')
+    else {
+      alert('Please fill Manager Id')
     }
-    
+
   }
-  display(){
-    if(!this.state.isReply){
+  display() {
+    if (!this.state.isReply) {
       return (
-          <ScrollView>
+        <ScrollView>
           <View style={styles.container}>
-              <Text style={styles.text_container}>Manager Id</Text>
-        <TextInput
-        style={styles.input}
-        value={this.state.managerId}
-        placeholderTextColor='grey'
-        onChangeText={val => this.onChangeText('managerId', val)}
-      />
-      <Text style={styles.text_container}>Member Id</Text>
-      <TextInput
-        style={styles.input}
-        value={this.state.memberId}
-        placeholderTextColor='grey'
-        onChangeText={val => this.onChangeText('memberId', val)}
-      />
-      </View>
-      </ScrollView>
+            <Text style={styles.text_container}>Manager Id</Text>
+            <TextInput
+              style={styles.input}
+              value={this.state.managerId}
+              placeholderTextColor='grey'
+              onChangeText={val => this.onChangeText('managerId', val)}
+            />
+            <Text style={styles.text_container}>Member Id</Text>
+            <TextInput
+              style={styles.input}
+              value={this.state.memberId}
+              placeholderTextColor='grey'
+              onChangeText={val => this.onChangeText('memberId', val)}
+            />
+          </View>
+        </ScrollView>
       )
     }
-    else{
+    else {
       return null
     }
   }
   render() {
     return (
-        <ScrollView>
-      <View style={styles.container}>
-        {this.display()}
-        <Text style={styles.text_container}>Report Id</Text>
-        <TextInput
-        style={styles.input}
-        placeholderTextColor='grey'
-        value={this.state.reportId}
-        onChangeText={val => this.onChangeText('reportId', val)}
-      />
-      <Text style={styles.text_container}>Report Text</Text>
-        <TextInput
-          style={styles.report_text}
-          value={this.state.reportText}
-          multiline={true}
-          placeholderTextColor='grey'
-          onChangeText={val => this.onChangeText('reportText', val)}
-        />
-        <Text style={styles.text_container}>Past Report Id</Text>
-        <TextInput
-          style={styles.input}
-          value={this.state.pastReportId}
-          placeholderTextColor='grey'
-          onChangeText={val => this.onChangeText('pastReportId', val)}
-        />
-        
-        <Button
-        style={{
-            alignItems : 'center'
-         }}
-          title='Send'
-          onPress={this.sendReply}
-        />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          {this.display()}
+          <Text style={styles.text_container}>Report Id</Text>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor='grey'
+            value={this.state.reportId}
+            onChangeText={val => this.onChangeText('reportId', val)}
+          />
+          <Text style={styles.text_container}>Report Text</Text>
+          <TextInput
+            style={styles.report_text}
+            value={this.state.reportText}
+            multiline={true}
+            placeholderTextColor='grey'
+            onChangeText={val => this.onChangeText('reportText', val)}
+          />
+          <Text style={styles.text_container}>Past Report Id</Text>
+          <TextInput
+            style={styles.input}
+            value={this.state.pastReportId}
+            placeholderTextColor='grey'
+            onChangeText={val => this.onChangeText('pastReportId', val)}
+          />
+
+          <Button
+            style={{
+              alignItems: 'center'
+            }}
+            title='Send'
+            onPress={this.sendReply}
+          />
+        </View>
       </ScrollView>
     )
   }
@@ -170,7 +167,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '500',
   },
-  report_text:{
+  report_text: {
     width: 350,
     height: 100,
     backgroundColor: 'white',
@@ -180,11 +177,11 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '500',
   },
-  text_container:{
-      marginTop:10,
-        marginLeft:12,
-        fontSize:15,
-        fontWeight:'bold'
+  text_container: {
+    marginTop: 10,
+    marginLeft: 12,
+    fontSize: 15,
+    fontWeight: 'bold'
   },
   container: {
     flex: 0,
